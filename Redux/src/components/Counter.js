9033de1e90903ca76badfce26d55f36f7e564de3
zyz <zyz951123@gmail.com>
2021-08-27 14:50:08 -0700

    Use redex for the auth logic

--- Redux/src/components/Counter.js
+++ Redux/src/components/Counter.js
@@ -1,29 +1,38 @@
 //useSelect is more convenient than useStore
 import { useSelector, useDispatch } from "react-redux";
 
+import { counterActions } from "../store/counter";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
   //Using useSelector will automatically subscribe to the redux store for the the component
-  const counter = useSelector((state) => state.counter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(10)); // { type: SOME_UNIQUE_IDENTIFIER, payload: 10 }
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -1,20 +1,10 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-// previous state and action for the arguments
-// Based on the action type, return a new state snapshot
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1 };
-  }
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
-  }
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
-  return state;
-};
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
 
-// Create the redux store, reducer must be the argument in the createStore function
-const store = createStore(counterReducer);
-
-//provide the redux store to the app component
 export default store;
